3df49c4f87965efe91d7e4654bb123546bfb10d1
drinkoder <drinkoder@gmail.com>
2020-07-22 03:12:41 +0100

    second commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const db_url = process.env.DATABASE_URL;
 
 mongoose.connect(db_url, { useNewUrlParser: true, useUnifiedTopology: true });
